a32958e13b8812b1a9e99f2a49e0b5e7c8e7dbfd
Prasannakbhat123 <prasannabhat@gmail.com>
2024-11-29 13:31:33 +0530

    changed no of clients

--- src/pages/about.js
+++ src/pages/about.js
@@ -99,7 +99,7 @@ const About = () => {
             <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
               <div className="flex flex-col items-end justify-center xl:items-center ">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={5} />+
+                  <AnimatedNumbers value={10} />+
                 </span>
                 <h2
                   className="text-xl font-medium capitalize text-dark/75
